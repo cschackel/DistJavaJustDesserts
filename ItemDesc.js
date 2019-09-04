8b1ccc265419c96fca96271050bcebac94acf53d
Cyrus Schackel <cschackel@my.wctc.edu>
2019-09-04 13:17:45 -0500

    Changeover to IntelliJ

--- ItemDesc.js
+++
@@ -1,9 +0,0 @@
-var commentBtn = document.getElementById("new-comment-btn");
-
-commentBtn.addEventListener("click",function()
-{
-    let form = document.getElementById("new-comment-form-box");
-    form.setAttribute("class","display-block");
-    commentBtn.setAttribute("class","hidden");
-});
-
